d699f859e98fdcb957ed643cf0bcd8292c5da564
Michael Smith <mikesmiffy128@gmail.com>
2021-02-08 14:22:48 +0000

    Make compiler detection smarter

--- scripts/cc-conf.sh
+++ scripts/cc-conf.sh
@@ -1,40 +1,138 @@
-_clang_ver="11.0.0"
-
 extra_ldflags=
 extra_cflags=
 can_sanitize=0
 
-case "$cc" in
-	*gcc)
-		extra_cflags="-flto"
-		extra_ldflags="-flto"
-		can_sanitize=1 ;;
-	clang*)
-		extra_cflags="-flto"
-		extra_ldflags="-flto"
-		can_sanitize=1
-		_havever="`clang --version | grep "clang version "`"
-		if [ "$_havever" != "clang version $_clang_ver" ]; then
-			warn "note: you have $_havever, but the project primarily uses $_clang_ver"
-			# XXX this assumes we're newer, technically we could be older briefly
-			# if I don't pacman -Syu for a while :^)
-			warn "* if you want a reproducible build, you need the exact version! *"
+cc_type="`echo "
+#if defined(__clang__)
+clang
+#elif defined(__GNUC__)
+gcc
+#elif defined(__TINYC__)
+tcc
+#else
+unknown
+#endif
+" | $cc -E - | tail -n1`"
+
+version_clang() {
+	cc_ver=
+	_suffix=
+	_prefix=
+	_split() {
+		# catch "FreeBSD Clang," "OpenBSD Clang," "Apple Clang," etc
+		while [ $1 != "clang" ]; do
+			_suffix="$_suffix-`echo "$1" | awk '{print tolower($0)}'`"
+			_prefix="$1 "
+			shift
+		done
+		shift 2 # clang version ____
+		cc_ver="$1$_suffix"
+	}
+	_split $("$cc" --version | head -1)
+	cc_friendly="${_prefix}Clang $cc_ver"
+}
+
+version_gcc() {
+	cc_ver=
+	_suffix=
+	_prefix=
+	_split() {
+		shift # first word should always be gcc
+		# HACK: hardcode nb4 yyyymmdd (do *other* OSes have GCC forks?)
+		if [ "$1" = "(nb4" ]; then
+			shift 2
+			_suffix="-netbsd"
+			_prefix="NetBSD "
+		else
+			shift # (GCC)
 		fi
-		# XXX for now, only trying LLD with Clang as GCC seems to have trouble
-		# using it (undefined symbol main) - other things may not even try to
-		# use it anyway e.g. tcc just ignores
-		if is_cmd ld.lld; then
+		while [ $# != 1 ]; do shift; done
+		cc_ver="$1$_suffix"
+	}
+	_split $("$cc" --version | head -1)
+	cc_friendly="${_prefix}GCC $cc_ver"
+}
+
+version_tcc() {
+	_split() {
+		shift 2 # tcc version ____
+		cc_ver="$1"
+	}
+	_split $("$cc" -version)
+	cc_friendly="tcc $cc_ver"
+}
+
+version_unknown() {
+	cc_ver="0"
+	cc_friendly="an unknown compiler"
+}
+
+eval "version_$cc_type"
+
+# to make it as easy as possible for users to reproduce builds (and to make it
+# as easy as possible for me to test-compile on supported platforms), use the
+# native compiler that ships will each stable OS release - except on linux,
+# since every distro is different. in that case I happen to use the latest
+# stable clang, so that's the canonical preferred linux compiler
+plat_supported_cc=
+case "$target_os" in
+	linux)
+		plat_supported_cc="clang-11.0.0"
+		plat_friendly_cc="Clang 11.0.0" ;;
+	freebsd)
+		plat_supported_cc="clang-10.0.1-freebsd"
+		plat_friendly_cc="FreeBSD Clang 10.0.1" ;;
+	openbsd)
+		plat_supported_cc="clang-10.0.1-openbsd"
+		plat_friendly_cc="OpenBSD Clang 10.0.1" ;;
+	netbsd)
+		plat_supported_cc="gcc-7.5.0-netbsd"
+		plat_friendly_cc="NetBSD GCC 7.5.0" ;;
+	# TODO(port) want illumos here too! can't get a test vm running atm though
+	*) ;;
+esac
+
+if [ "$plat_supported_cc" != "" -a \
+		"$cc_type-$cc_ver" != "$plat_supported_cc" ]; then
+	warn "note: compiling using $cc_friendly rather than $plat_friendly_cc,"
+	warn "      which is the supported compiler for your target platform"
+	warn "* to reproduce a build that definitely works, use $plat_friendly_cc!"
+fi
+
+case "$cc_type" in
+	clang)
+		extra_cflags="-flto -fpic"
+		extra_ldflags="-flto -fpic"
+		can_sanitize=1
+		# for now, only trying LLD with Clang as GCC seems to have trouble using
+		# it (undefined symbol main) - other things may not even try to use it
+		# anyway e.g. tcc just ignores
+		if [ "$target_os" = macos ]; then
+			: # oh, don't use lld on macos though, it doesn't work
+		elif is_cmd ld.lld; then
 			extra_ldflags="$extra_ldflags -fuse-ld=lld"
 		else
-			warn "note: ld.lld is unavailable, using system default linker instead"
-			warn "      This is probably fine, but the project primarily uses LLD"
-			warn "* if you want a reproducible build, you might need to install lld! *"
+			warn "note: lld is unavailable, using system default linker instead"
+			warn "* this should still work, but the project normally uses lld with Clang"
+			warn "* to reproduce a build that definitely works, install lld!"
 		fi ;;
+	gcc)
+		extra_cflags="-flto -fpic"
+		extra_ldflags="-flto -fpic"
+		can_sanitize=1 ;;
 	tcc)
 		if [ "$target_os" = "linux" ]; then
+			# HACK: link an extra file; see comment in that file for insight
 			extra_ldflags="$extra_ldflags scripts/tcchack.c"
 		fi ;;
 	*) ;;
 esac
+
+# see DevDocs/pie.txt for this part
+pie=
+if [ "$cc_type" = gcc -o "$cc_type" = clang ] && \
+		[ $target_os != "freebsd" ]; then
+	pie="-pie"
+fi
 
 # vi: sw=4 ts=4 noet tw=80 cc=80
--- scripts/cc-pick.sh
+++ scripts/cc-pick.sh
@@ -1,14 +1,26 @@
-if is_cmd clang; then
-	cc=clang
-elif is_cmd gcc; then
-	warn "note: Clang is unavailable, using GCC instead"
-	warn "      This is probably fine, but the project primarily uses Clang"
-	warn "* if you want a reproducible build, install Clang $_clang_ver! *"
-	cc=gcc
+cc=cc
+
+# most platforms use clang these days but netbsd and illumos still ship GCC
+# we want to use the standard host compiler as the preferred compiler by
+# default, assuming no cross-compiling (if the user wants to cross-compile they
+# will need to set a specific $CC, so this logic here won't matter)
+_uname="`uname`"
+if [ "$_uname" = NetBSD -o "$_uname" = SunOS ]; then
+	_try="gcc clang"
 else
+	_try="clang gcc"
+fi
+
+for _t in $_try; do
+	if is_cmd $_t; then
+		cc=$_t
+		break
+	fi
+done
+
+if [ "$cc" = cc ]; then
 	warn 'warning: unsure what C compiler to use, just using `cc`'
-	warn "* if something fails to build, install Clang $_clang_ver! *"
-	cc=cc
+	warn "* if something fails to build, install Clang or GCC! *"
 fi
 
 # vi: sw=4 ts=4 noet tw=80 cc=80
